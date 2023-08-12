e2b9e6f5a5a2008a515a2cd596b36b844f0451c3
Dairo estiven santana charry <dairosantab@gmail.com>
2023-08-12 17:34:09 -0500

    se subieron los archivos

---
+++ js/inicio.js
@@ -0,0 +1,22 @@
+let contador = 0;
+while (contador < 10) {
+    console.log("contado: " + contador);
+    contador++;
+}
+
+// funciones 
+function saludar(nombre) {
+    console.log("Hola " + nombre);
+}
+
+saludar("dairo")
+
+function operacion(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(operacion(4, 4));
+
+for (let i = 0; i < 100; i+2) {
+
+}    
